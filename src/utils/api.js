ffbbdc44561b4fd6099c89be8c729df6ebc852c2
tyrese-s <tyrese.sylvester@yahoo.com>
2023-03-16 10:22:26 +0000

    added optimistic voting to reviews

--- src/utils/api.js
+++ src/utils/api.js
@@ -18,3 +18,12 @@ export const getComments = (review_id) => {
     return reviewsApi.get(`/reviews/${review_id}/comments`).then(({data}) => {
         return data}
 )}   
+
+export const voteForReview = (review_id) => {
+    return reviewsApi.patch(`/reviews/${review_id}`, {
+        inc_votes: 1
+    })
+    .then(({ data }) => {
+        return data
+    })
+}
