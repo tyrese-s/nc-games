23250dbdd222e7ef65c1c013d4b0680a43c95fa9
tyrese-s <tyrese.sylvester@yahoo.com>
2023-04-21 19:01:26 +0100

    some styling improvements

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,15 +3,18 @@ import { UserContext } from "../context/User"
 
 function Nav (){
 
-    return (<div>
-        <Link to="/">Home</Link>
-        <br />
-        <br />
-        <Link to="/reviews">Reviews</Link>
-        <UserContext.Consumer>
-            {value =><h3> loged in as: {value.user.username} </h3>}
-        </UserContext.Consumer>
-    </div>
+    return (
+        <div>
+            <div className="nav_bar">
+                <Link className="nav_button" to="/">Home</Link>
+                <br/>
+                <br/>
+                <Link className="nav_button" to="/reviews">Reviews</Link>
+            </div>
+                <UserContext.Consumer>
+                    {value =><h3> loged in as: {value.user.username} </h3>}
+                </UserContext.Consumer>
+        </div>
     )
 }
 export default Nav
